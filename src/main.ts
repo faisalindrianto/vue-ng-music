3a8a2a41505949d21ee0c06e116e8a5149c92bba
Faisal Indrianto <wwf051099@gmail.com>
2022-07-25 16:32:10 +0700

    setup mdivue for icon

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import '@/assets/tailwind.css'
 import '@/assets/style.css'
+import mdiVue from 'mdi-vue/v3'
+import * as mdijs from '@mdi/js'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +12,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(mdiVue, {
+  icons: mdijs
+})
 
 app.mount('#app')
